56a120aed49af2d1b2edfc50ba59e700c76678ec
小瑞 <89796393+bestruirui@users.noreply.github.com>
2022-12-31 13:24:24 +0000

    修改一些内容

--- public/config.js
+++ public/config.js
@@ -1,36 +1,28 @@
 window.Config = {
 
   // 显示标题
-  SiteName: 'Public Status',
+  SiteName: '小瑞',
 
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
-    'm784488775-dd1ad84b209c05f8e185c33e',
-    'm784490063-7b5da437e7f1e0d67613714d',
-    'm784497419-de55aa09902ccb3ab22d548a',
-    'm784496436-71a4bf7b1e3bdf7756be131b',
+    'm793367122-7b29afdd7ed255d574174e80',
+    'm793367142-e17124847e3e362da2787a28',
+    'm793367164-5c95ccfe13d60d821811cf0c',
+    'm793371331-0aacae57b126cd223048424b',
   ],
 
   // 日志天数
-  CountDays: 90,
+  CountDays: 60,
 
   // 是否显示检测站点的链接
   ShowLink: true,
 
   // 导航栏菜单
   Navi: [
-    {
-      text: 'Homepage',
-      url: 'https://status.org.cn/'
-    },
-    {
-      text: 'GitHub',
-      url: 'https://github.com/yb/uptime-status'
-    },
-    {
-      text: 'Blog',
-      url: 'https://abo.xyz/'
-    },
+    // {
+    //   text: 'Homepage',
+    //   url: 'https://bestrui.top'
+    // },
   ],
 };
--- src/components/link.js
+++ src/components/link.js
@@ -1,8 +1,7 @@
 function Link(props = { text, to }) {
   return (
-    <a {...props} href={props.to} target='_blank'>
-      {props.text}
-    </a>
+     <a >
+     </a>
   );
 }
 
