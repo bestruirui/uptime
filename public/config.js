62989f06b71d6a13895b008b6494da71557328fc
小瑞 <89796393+bestruirui@users.noreply.github.com>
2023-01-01 05:03:56 +0000

    增加知识库监控

--- public/config.js
+++ public/config.js
@@ -7,9 +7,11 @@ window.Config = {
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
     'm793367122-7b29afdd7ed255d574174e80',
-    'm793367142-e17124847e3e362da2787a28',
-    'm793367164-5c95ccfe13d60d821811cf0c',
+    
     'm793371331-0aacae57b126cd223048424b',
+    'm793367142-e17124847e3e362da2787a28',
+    'm793377122-f88d71850d8bd43344b064cd',
+    'm793367164-5c95ccfe13d60d821811cf0c',
   ],
 
   // 日志天数
