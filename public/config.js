ba3e2ceb67e3b1073fd90da6a49170466c0402a5
小瑞 <89796393+bestruirui@users.noreply.github.com>
2023-01-12 13:47:47 +0000

    增加一些服务

--- public/config.js
+++ public/config.js
@@ -6,11 +6,14 @@ window.Config = {
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
-    'm793367122-7b29afdd7ed255d574174e80',
-    'm793371331-0aacae57b126cd223048424b',
-    'm793367142-e17124847e3e362da2787a28',
-    'm793377122-c18d7313bc7de7ee3a9027ef',
-    'm793377775-f73841ef05fa209816f0201a',
+    'm793367122-7b29afdd7ed255d574174e80',//个人主页
+    'm793377122-c18d7313bc7de7ee3a9027ef',//知识库
+    'm793371331-0aacae57b126cd223048424b',//评论服务
+    'm793367142-e17124847e3e362da2787a28',//京东登录
+    'm793459061-6bba3a9193b3a9eafb577fd2',//网站截图
+    'm793459042-290866b557decff10d76baee',//WebRec
+    'm793377775-f73841ef05fa209816f0201a',//Alist
+    'm793459032-ecf03a4ffc4a0fdda25d0eff',//SnapDrop
   ],
 
   // 日志天数
